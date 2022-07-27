2e11e3c6c6ea2d1350ab99205bf2923617a7fc49
dao vo <gary.vo32@gmail.com>
2022-07-26 22:00:31 -0700

    created util folder and added ExpressError and wrapAsync within to help handle errors when validating

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require('./utils/wrapAsync')
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
@@ -38,20 +39,16 @@ app.get("/campgrounds/new", (req, res) => {
   res.render("campgrounds/new");
 });
 
-app.post("/campgrounds", async (req, res, next) => {
-  try {
+app.post("/campgrounds", wrapAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect("/campgrounds/" + campground._id);
-  } catch (err) {
-    next(err);
-  }
-});
+}));
 
-app.get("/campgrounds/:id", async (req, res) => {
+app.get("/campgrounds/:id", wrapAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render("campgrounds/show", { campground });
-});
+}));
 
 app.get("/campgrounds/:id/edit", async (req, res) => {
   const campground = await Campground.findById(req.params.id);
